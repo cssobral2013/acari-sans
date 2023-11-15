bb5ffe306ea9d4796e2c7114dfcb582fd4036782
Cristiano Sobral <cssobral2013@gmail.com>
2023-11-14 22:38:43 -0300

    Update v1.037

---
+++ fonts/generate-woff-woff2.py
@@ -0,0 +1,291 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/AcariSans-Light.ttf'
+woffPath = './web/AcariSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/AcariSans-LightItalic.ttf'
+woffPath = './web/AcariSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/AcariSans-Book.ttf'
+woffPath = './web/AcariSans-Book.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-BookItalic.ttf'
+woffPath = './web/AcariSans-BookItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+    
+srcPath  = './ttf/AcariSans-Regular.ttf'
+woffPath = './web/AcariSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-Italic.ttf'
+woffPath = './web/AcariSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-Medium.ttf'
+woffPath = './web/AcariSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-MediumItalic.ttf'
+woffPath = './web/AcariSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBold.ttf'
+woffPath = './web/AcariSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBoldItalic.ttf'
+woffPath = './web/AcariSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBold.ttf'
+woffPath = './web/AcariSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBoldItalic.ttf'
+woffPath = './web/AcariSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-Bold.ttf'
+woffPath = './web/AcariSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-BoldItalic.ttf'
+woffPath = './web/AcariSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-ExtraBold.ttf'
+woffPath = './web/AcariSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-ExtraBoldItalic.ttf'
+woffPath = './web/AcariSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-Black.ttf'
+woffPath = './web/AcariSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/AcariSans-BlackItalic.ttf'
+woffPath = './web/AcariSans-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+
+srcPath  = './ttf/AcariSans-Light.ttf'
+woff2Path = './web/AcariSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/AcariSans-LightItalic.ttf'
+woff2Path = './web/AcariSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/AcariSans-Book.ttf'
+woff2Path = './web/AcariSans-Book.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-BookItalic.ttf'
+woff2Path = './web/AcariSans-BookItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+     
+
+    
+srcPath  = './ttf/AcariSans-Regular.ttf'
+woff2Path = './web/AcariSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-Italic.ttf'
+woff2Path = './web/AcariSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-Medium.ttf'
+woff2Path = './web/AcariSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-MediumItalic.ttf'
+woff2Path = './web/AcariSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBold.ttf'
+woff2Path = './web/AcariSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBoldItalic.ttf'
+woff2Path = './web/AcariSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBold.ttf'
+woff2Path = './web/AcariSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-SemiBoldItalic.ttf'
+woff2Path = './web/AcariSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-Bold.ttf'
+woff2Path = './web/AcariSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-BoldItalic.ttf'
+woff2Path = './web/AcariSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-ExtraBold.ttf'
+woff2Path = './web/AcariSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-ExtraBoldItalic.ttf'
+woff2Path = './web/AcariSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-Black.ttf'
+woff2Path = './web/AcariSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/AcariSans-BlackItalic.ttf'
+woff2Path = './web/AcariSans-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
